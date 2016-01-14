3c1c95bd998314d66fcfbf7d62cd41513978115d
reedyseth <reedyseth@gmail.com>
2016-01-14 06:44:22 -0700

    Update reference to function subscribe_reloaded_manage()

--- subscribe.php
+++ subscribe.php
@@ -1,7 +1,7 @@
 
 <?php
 /* Template Name: Subscribe To Comments */
-if (isset($wp_subscribe_reloaded)){ global $posts; $posts = $wp_subscribe_reloaded->subscribe_reloaded_manage(); }
+if (isset($wp_subscribe_reloaded)){ global $posts; $posts = $wp_subscribe_reloaded->stcr->subscribe_reloaded_manage(); }
 get_header(); ?>
 <!--page.php-->
 <div id="content">
